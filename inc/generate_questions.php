6eda9be7ffd5c2ca0150778bfc7dad8c6aa81de6
Charles Seymour <charlesseymour@yahoo.com>
2019-08-01 14:57:33 -0500

    Generate questions

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,13 +1,41 @@
 <?php
 // Generate random questions
 
+$questions = [];
+
 // Loop for required number of questions
 
-// Get random numbers to add
+for ($i = 0; $i < 10; $i++) {
 
-// Calculate correct answer
+  // Get random numbers to add
 
-// Get incorrect answers within 10 numbers either way of correct answer
-// Make sure it is a unique answer
+  $questions[$i]["leftAdder"] = rand(1, 99);
+  $questions[$i]["rightAdder"] = rand(1, 99);
 
-// Add question and answer to questions array
+  // Calculate correct answer
+
+  $questions[$i]["correctAnswer"] = $questions[$i]["leftAdder"] +
+                                    $questions[$i]["rightAdder"];
+
+  // Get incorrect answers within 10 numbers either way of correct answer
+  // Make sure it is a unique answer
+
+  do {
+    $n = rand($questions[$i]["correctAnswer"] - 10,
+              $questions[$i]["correctAnswer"] + 10);
+  } while (
+    in_array($n, array($questions[$i]["correctAnswer"]))
+  );
+
+  $questions[$i]["firstIncorrectAnswer"] = $n;
+
+  do {
+    $n = rand($questions[$i]["correctAnswer"] - 10,
+              $questions[$i]["correctAnswer"] + 10);
+  } while (
+    in_array($n, array($questions[$i]["correctAnswer"],
+                       $questions[$i]["firstIncorrectAnswer"]))
+  );
+
+  $questions[$i]["secondIncorrectAnswer"] = $n;
+}
